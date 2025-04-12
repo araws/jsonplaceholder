2700add0a2b0e750a29db3a246d5ab9525be711f
Andrzej Rawski <rawski.andrzej@gmail.com>
2025-04-12 10:34:36 +0200

    Created test for read users with path variable and query params

--- src/test/java/JsonplaceholderGETTest.java
+++ src/test/java/JsonplaceholderGETTest.java
@@ -38,4 +38,39 @@ class JsonplaceholderGETTest {
         Assertions.assertEquals("Sincere@april.biz", json.get("email"));
         Assertions.assertEquals("Kulas Light", json.get("address.street"));
     }
+
+    // PATH VARIABLES
+
+    @Test
+    void jsonplaceholderReadOneUserWithPathVariable(){
+        Response response = given()
+                .pathParam("userId", 1)
+                .when()
+                .get("https://jsonplaceholder.typicode.com/users/{userId}");
+
+        JsonPath json = response.jsonPath();
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals("Leanne Graham", json.get("name"));
+        Assertions.assertEquals("Sincere@april.biz", json.get("email"));
+        Assertions.assertEquals("Kulas Light", json.get("address.street"));
+    }
+
+    // QUERY PARAMS
+
+    @Test
+    void jsonplaceholderReadUsersWithQueryParams(){
+        Response response = given()
+                .queryParam("username", "Bret")
+                .when()
+                .get("https://jsonplaceholder.typicode.com/users/");
+
+        JsonPath json = response.jsonPath();
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals("Leanne Graham", json.getList("name").get(0));
+        Assertions.assertEquals("Bret", json.getList("username").get(0));
+        Assertions.assertEquals("Sincere@april.biz", json.getList("email").get(0));
+        Assertions.assertEquals("Kulas Light", json.getList("address.street").get(0));
+    }
 }
