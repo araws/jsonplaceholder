172de214b4c347dd34e2c34e24a9ea46a2b86875
Andrzej Rawski <rawski.andrzej@gmail.com>
2025-04-12 10:21:47 +0200

    Created test for read one user

--- src/test/java/JsonplaceholderGETTest.java
+++ src/test/java/JsonplaceholderGETTest.java
@@ -1,6 +1,9 @@
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -11,8 +14,28 @@ class JsonplaceholderGETTest {
         Response response = given()
                 .when()
                 .get("https://jsonplaceholder.typicode.com/users");
-        System.out.println(response.asString());
 
         Assertions.assertEquals(200, response.statusCode());
+
+        JsonPath json = response.jsonPath();
+
+        List<String> names = json.getList("name");
+
+        Assertions.assertEquals(10, names.size());
+
+    }
+
+    @Test
+    void jsonplaceholderReadOneUser(){
+        Response response = given()
+                .when()
+                .get("https://jsonplaceholder.typicode.com/users/1");
+
+        JsonPath json = response.jsonPath();
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals("Leanne Graham", json.get("name"));
+        Assertions.assertEquals("Sincere@april.biz", json.get("email"));
+        Assertions.assertEquals("Kulas Light", json.get("address.street"));
     }
 }
