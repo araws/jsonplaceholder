5481012f46c82521a7063118bdc5e2233191ac86
Andrzej Rawski <rawski.andrzej@gmail.com>
2025-04-12 12:58:35 +0200

    Updated string json to JSONObject

--- src/test/java/JsonplaceholderPUTPATCHTest.java
+++ src/test/java/JsonplaceholderPUTPATCHTest.java
@@ -1,5 +1,6 @@
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -9,33 +10,36 @@ class JsonplaceholderPUTPATCHTest {
 
     @Test
     void jsonplaceholderUpdateUserPUTTest(){
-        String jsonBody = """
-                {
-                    "name": "Andrzej Update PUT",
-                    "username": "AndrzejPUT",
-                    "email": "andrzejPUT@arit.pl",
-                    "address": {
-                        "street": "Kulas Light",
-                        "suite": "Apt. 556",
-                        "city": "Gwenborough",
-                        "zipcode": "92998-3874",
-                        "geo": {
-                            "lat": "-37.3159",
-                            "lng": "81.1496"
-                        }
-                    },
-                    "phone": "1-770-736-8031 x56442",
-                    "website": "hildegard.org",
-                    "company": {
-                        "name": "Romaguera-Crona",
-                        "catchPhrase": "Multi-layered client-server neural-net",
-                        "bs": "harness real-time e-markets"
-                    }
-                }""";
+        JSONObject user = new JSONObject();
+        user.put("name", "Andrzej Update PUT");
+        user.put("username", "AndrzejPUT");
+        user.put("email", "andrzejPUT@arit.pl");
+        user.put("phone", "1-770-736-8031 x56442");
+        user.put("website", "hildegard.org");
+
+        JSONObject geo = new JSONObject();
+        geo.put("lat", "-37.3159");
+        geo.put("lng", "81.1496");
+
+        JSONObject address = new JSONObject();
+        address.put("street", "Kulas Light");
+        address.put("street", "Kulas Light");
+        address.put("city", "Gwenborough");
+        address.put("zipcode", "92998-3874");
+        address.put("geo", geo);
+
+        user.put("address", address);
+
+        JSONObject company = new JSONObject();
+        company.put("name", "Romaguera-Crona");
+        company.put("catchPhrase", "Multi-layered client-server neural-net");
+        company.put("bs", "harness real-time e-markets");
+
+        user.put("company", company);
 
         Response response = given()
                 .contentType("application/json")
-                .body(jsonBody)
+                .body(user.toString())
                 .when()
                 .put("https://jsonplaceholder.typicode.com/users/1")
                 .then()
@@ -52,14 +56,13 @@ class JsonplaceholderPUTPATCHTest {
 
     @Test
     void jsonplaceholderUpdateUserPATCHTest(){
-        String jsonBody = """
-                {
-                    "email": "andrzejPATCH@arit.pl"
-                }""";
+
+        JSONObject userDetails = new JSONObject();
+        userDetails.put("email", "andrzejPATCH@arit.pl");
 
         Response response = given()
                 .contentType("application/json")
-                .body(jsonBody)
+                .body(userDetails.toString())
                 .when()
                 .patch("https://jsonplaceholder.typicode.com/users/1")
                 .then()
